2eb5f1ab377fbc607ca2b51762be0c3644747b1a
prodigysurge <boatinglife6@yahoo.com>
2021-01-14 22:21:49 -0500

    commit tree f50dc0c39af7827c3f3b18bc96cd671bc863317c

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,63 @@
+def sort_array_asc(arr)
+    arr.sort do |a,b|
+        if a == b 
+            0
+        elsif a < b
+            -1
+        elsif a > b
+            1
+        end
+    end
+end
+
+def sort_array_desc(arr)
+    arr.sort do |b,a|
+        if a == b 
+            0
+        elsif a < b 
+            -1
+        elsif a > b 
+            1
+        end
+    end
+end
+
+def sort_array_char_count(arr)
+    arr = arr.sort_by {|a| a.length}
+end
+
+def swap_elements(arr)
+    arr[1], arr[2] = arr[2], arr[1]
+    arr
+end
+
+def reverse_array(arr)
+    arr.reverse
+end
+
+def kesha_maker(arr)
+    arr.each {|j|j[2] = "$"}
+   
+end
+
+def find_a(arr)
+    arr.select do |element|
+      element[0] == "a"
+    end
+end
+
+def sum_array(arr)
+    arr.sum
+end
+
+def add_s(arr)
+    arr.map do |element|
+        if element != arr[1]
+            element = element + "s"
+        else 
+            element
+        end
+    end
+end
+
 
